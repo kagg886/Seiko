a277865c77c207812878866c832a30266e6cff43
kagg886 <iveour@163.com>
2023-03-19 14:39:03 +0800

    feat(扫码登录): ✨ 优化扫码对话框

--- app/src/main/java/com/kagg886/seiko/adapter/LogAdapter.java
+++ app/src/main/java/com/kagg886/seiko/adapter/LogAdapter.java
@@ -53,11 +53,9 @@ public class LogAdapter extends BaseAdapter {
                         while (reader.read(buf) != -1) {
                             if (buf[0] == SPLIT_STR) {
                                 if (isCollected) { //遇到配对的符号了，切割然后合成大字符串
-
                                     Bundle b = new Bundle();
-                                    b.putString("str",builder.substring(3));
+                                    b.putString("str", builder.substring(3));
                                     Message msg = new Message();
-                                    msg.what = 0;
                                     msg.setData(b);
                                     mHandler.sendMessage(msg);
                                     builder = new StringBuilder();
--- app/src/main/java/com/kagg886/seiko/bot/AndroidSolver.java
+++ app/src/main/java/com/kagg886/seiko/bot/AndroidSolver.java
@@ -1,5 +1,6 @@
 package com.kagg886.seiko.bot;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -11,13 +12,19 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import androidx.activity.result.ActivityResult;
 import androidx.annotation.NonNull;
+import com.kagg886.seiko.R;
 import com.kagg886.seiko.activity.CaptchaActivity;
 import com.kagg886.seiko.activity.MainActivity;
 import com.kagg886.seiko.activity.SMSActivity;
 import com.kagg886.seiko.event.SnackBroadCast;
+import com.kagg886.seiko.util.IOUtil;
+import com.kagg886.seiko.util.ShareUtil;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
 import kotlin.coroutines.CoroutineContext;
@@ -34,6 +41,13 @@ import net.mamoe.mirai.utils.LoginSolver;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 /**
  * @projectName: Seiko
  * @package: com.kagg886.seiko.bot
@@ -43,15 +57,18 @@ import org.jetbrains.annotations.Nullable;
  * @date: 2022/12/13 12:31
  * @version: 1.0
  */
-public class AndroidSolver extends LoginSolver implements QRCodeLoginListener {
+public class AndroidSolver extends LoginSolver implements QRCodeLoginListener, View.OnClickListener {
 
     private final MainActivity avt;
 
     //扫码登录的Dialog
     private AlertDialog dialog;
 
-    //扫码登录是否被取消
-    private boolean cancel = false;
+    //是否取消登录
+    private int cancelType = 0;
+
+    //扫码登录的二维码图片
+    private Bitmap bitmap;
 
     @Override
     public int getQrCodeSize() {
@@ -73,14 +90,14 @@ public class AndroidSolver extends LoginSolver implements QRCodeLoginListener {
                 case 1:
                     State state = State.valueOf(msg.getData().getString("state"));
                     switch (state) {
-                        case CANCELLED:
-                            SnackBroadCast.sendBroadCast("用户主动取消扫码，登录取消");
-                            dialog.dismiss();
-                            throw new UnsupportedQRCodeCaptchaException("扫码登录已取消");
                         case TIMEOUT:
-                            SnackBroadCast.sendBroadCast("未在规定时间内扫码，登录取消");
-                            dialog.dismiss();
-                            throw new UnsupportedQRCodeCaptchaException("扫码登录已取消");
+                            cancelType = 2;
+                            dialog.cancel();
+                            break;
+                        case CANCELLED: //这里的取消是用户在手机qq上拒绝扫码登录
+                            cancelType = 1;
+                            dialog.cancel();
+                            break;
                         case WAITING_FOR_SCAN:
                             dialogController.sendEmptyMessage(0);
                             break;
@@ -88,7 +105,8 @@ public class AndroidSolver extends LoginSolver implements QRCodeLoginListener {
                             SnackBroadCast.sendBroadCast("扫码成功，等待客户端确认");
                             break;
                         case CONFIRMED:
-                            dialog.dismiss();
+                            bitmap.recycle();
+                            dialog.cancel();
                             break;
                     }
             }
@@ -107,28 +125,41 @@ public class AndroidSolver extends LoginSolver implements QRCodeLoginListener {
 
     @Override
     public void onFetchQRCode(@NotNull Bot bot, @NotNull byte[] bytes) {
-        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+        this.bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
         AlertDialog.Builder builder = new AlertDialog.Builder(avt);
         builder.setTitle("扫码登录:" + bot.getId());
-        ImageView i = new ImageView(avt);
-        i.setImageBitmap(bitmap);
-        builder.setView(i);
 
-        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                cancel = true;
-            }
-        });
+        View i = LayoutInflater.from(avt).inflate(R.layout.dialog_qrlogin,null);
+
+        ImageView imageView = i.findViewById(R.id.dialog_qrlogin_src);
+        imageView.setImageBitmap(bitmap);
+
+        Button cancel = i.findViewById(R.id.dialog_qrlogin_cancel);
+        Button share = i.findViewById(R.id.dialog_qrlogin_share);
+        cancel.setOnClickListener(this);
+        share.setOnClickListener(this);
+
+        builder.setView(i);
+        builder.setCancelable(false);
+
         avt.runOnUiThread(() -> dialog = builder.create());
     }
 
     @Override
     public void onIntervalLoop() {
         QRCodeLoginListener.super.onIntervalLoop();
-        if (cancel) {
-            throw new UnsupportedQRCodeCaptchaException("扫码登录被用户取消");
+        switch (cancelType) {
+            case 1:
+                throw new UnsupportedQRCodeCaptchaException("扫码登录已被用户取消");
+            case 2:
+                throw new UnsupportedQRCodeCaptchaException("超时，自动取消扫码");
+
         }
+//        if (cancelType != 0) {
+//            bitmap.recycle();
+//            dialog.cancel();
+//            throw new UnsupportedQRCodeCaptchaException("扫码登录被用户取消");
+//        }
     }
 
     @Override
@@ -197,5 +228,26 @@ public class AndroidSolver extends LoginSolver implements QRCodeLoginListener {
             throw new UnsupportedSliderCaptchaException("滑块验证被用户取消");
         }
         return result.getData().getStringExtra("ticket");
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.dialog_qrlogin_share:
+                File f = avt.getExternalFilesDir("tmp").toPath().resolve(UUID.randomUUID().toString().replace("-","") + ".png").toFile();
+                try {
+                    bitmap.compress(Bitmap.CompressFormat.PNG,90, Files.newOutputStream(f.toPath()));
+                } catch (IOException e) {
+                    SnackBroadCast.sendBroadCast("保存图片失败:" + e.getMessage());
+                }
+                ShareUtil.quickShare(avt,f,"image/*");
+                break;
+            case R.id.dialog_qrlogin_cancel:
+                v.setClickable(false);
+                cancelType = 1;  //这里的取消是在Seiko里取消扫码登录
+                dialog.cancel();
+                break;
+        }
     }
 }
