3623bedd96213b23f57acb681aeedc493ba1987c
kagg886 <iveour@163.com>
2023-04-17 16:25:01 +0800

    feat: ✨ 新增函数: '异步调用'

--- DictionaryCore/src/main/java/com/kagg886/seiko/dic/entity/func/impl/TransferDicMethod.java
+++ DictionaryCore/src/main/java/com/kagg886/seiko/dic/entity/func/impl/TransferDicMethod.java
@@ -5,6 +5,7 @@ import com.kagg886.seiko.dic.session.AbsRuntime;
 import com.kagg886.seiko.dic.session.impl.FunctionRuntime;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @projectName: Seiko
@@ -19,6 +20,36 @@ public class TransferDicMethod extends Function.UnInterruptedFunction {
 
     public TransferDicMethod(int line, String code) {
         super(line, code);
+    }
+
+
+    /**
+     * @projectName: Seiko
+     * @package: com.kagg886.seiko.dic.entity.func.uninterrupted
+     * @className: TransferDicMethod
+     * @author: kagg886
+     * @description: $异步调用 词库方法名 注册的键变量(可选)$
+     * @date: 2023/2/17 10:39
+     * @version: 1.0
+     */
+    public static class Async_Call extends TransferDicMethod {
+
+        public Async_Call(int line, String code) {
+            super(line, code);
+        }
+
+        @Override
+        protected void run(AbsRuntime<?> runtime, List<Object> args) {
+            Thread thread = new Thread(() -> {
+                FunctionRuntime dicRun = new FunctionRuntime(runtime.getFile(), runtime);
+                dicRun.invoke(args.get(0).toString());
+            });
+            thread.setName(UUID.randomUUID().toString());
+            if (args.size() >= 3) {
+                runtime.getRuntimeObject().put(args.get(2).toString(), thread.getName());
+            }
+            runtime.getFile().getExecutor().execute(thread);
+        }
     }
 
     @Override
