572848ebcf1c5a416b4bbec93698d6299ab54e07
kagg886 <iveour@163.com>
2023-05-01 16:14:21 +0800

    fix: 🐛 ConsolePlugin版本不变问题 feat: ✨ 增加mcl插件的检查更新

--- ConsolePlugin/src/main/java/com/kagg886/seiko/dic/mirai_console/PluginLoader.java
+++ ConsolePlugin/src/main/java/com/kagg886/seiko/dic/mirai_console/PluginLoader.java
@@ -1,15 +1,20 @@
 package com.kagg886.seiko.dic.mirai_console;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.kagg886.seiko.dic.DictionaryEnvironment;
 import com.kagg886.seiko.dic.DictionaryReg;
 import com.kagg886.seiko.dic.bridge.DictionaryListener;
+import com.kagg___.seiko.dic.mirai_console.ConsolePlugin.BuildConfig;
 import net.mamoe.mirai.console.command.CommandManager;
 import net.mamoe.mirai.console.extension.PluginComponentStorage;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.event.EventChannel;
 import net.mamoe.mirai.event.GlobalEventChannel;
+import net.mamoe.mirai.message.data.Message;
 import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
 
 import java.io.File;
 
@@ -27,7 +32,7 @@ public class PluginLoader extends JavaPlugin implements DictionaryListener {
     public static final PluginLoader INSTANCE = new PluginLoader();
 
     private PluginLoader() {
-        super(new JvmPluginDescriptionBuilder("com.kagg886.seiko.dic.mirai_console", "0.0.8")
+        super(new JvmPluginDescriptionBuilder("com.kagg886.seiko.dic.mirai_console", BuildConfig.DIC_VERSION)
                 .name("Seiko Dictionary Plugin")
                 .info("可以在PC上运行伪代码的插件\n项目地址:https://github.com/kagg886/Seiko")
                 .author("kagg886 and All Seiko Contributors")
@@ -49,6 +54,25 @@ public class PluginLoader extends JavaPlugin implements DictionaryListener {
         DictionaryEnvironment.getInstance().setErrorListener(this);
         DictionaryEnvironment.getInstance().setDicConfigPoint(getConfigFolderPath().resolve("dicList.json").toFile().getAbsolutePath());
         DictionaryEnvironment.getInstance().setDicData(tmp.toPath());
+
+        new Thread(() -> {
+            try {
+                JSONObject object = JSON.parseObject(
+                        Jsoup.connect("https://api.github.com/repos/kagg886/Seiko/releases/latest")
+                                .ignoreContentType(true)
+                                .timeout(10000)
+                                .execute().body());
+                String newVer = object.getString("tag_name");
+                if (!("V" + BuildConfig.DIC_VERSION).equals(newVer)) {
+                    getLogger().info(String.format("发现更新:%s->%s",BuildConfig.DIC_VERSION,newVer));
+                    getLogger().info(object.getString("body"));
+                    getLogger().info("下载地址:" + object.getString("html_url"));
+                }
+            } catch (Exception e) {
+                getLogger().error("更新检测失败!");
+            }
+        }).start();
+
         getLogger().info("Environment初始化成功!");
     }
 
